60092c71bca16916642e786d981c13555a1b58c5
Anh <tienanhquoc2005@gmail.com>
2026-05-22 15:50:56 +0700

    Update FE

--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
+import AccountManagementPage from '../pages/AccountManagementPage';
 import HomePage from '../pages/HomePage';
 import NotFoundPage from '../pages/NotFoundPage';
 import { ROUTES } from '../constants/routes';
@@ -7,6 +8,7 @@ export function AppRoutes() {
   return (
     <Routes>
       <Route path={ROUTES.HOME} element={<HomePage />} />
+      <Route path={ROUTES.ADMIN.USERS} element={<AccountManagementPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
